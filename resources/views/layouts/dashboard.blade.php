4a0a94cdd67f6683499c3b341cc1bd5a1edd4206
Fercho Posting <shaaf4ever@gmail.com>
2021-07-19 19:20:13 -0500

    Enable hot reload

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -256,7 +256,7 @@
     <script src="{{ asset('js/main.js') }}"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
 
 </body>
 </html>
